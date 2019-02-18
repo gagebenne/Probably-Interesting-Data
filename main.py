ce741582fdc34c66199cfb1df37e8b87a2408a7c
Gage Benne <gagedbenne@gmail.com>
2019-02-17 22:09:29 -0600

    Skeleton for additional dataset

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from cluster import kmeans
 
-print("K-means clustering")
-dataset = "datasets/Iris.csv"
+dataset = "datasets/iris.csv"
 xs = "SepalLengthCm"
 ys = "PetalLengthCm"
 k = 2
-
 kmeans(dataset, xs, ys, k)
+
+# dataset = 
+# xs =
+# ys =
+# k =
+# kmeans(dataset, xs, ys, k)
