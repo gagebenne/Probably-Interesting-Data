8e964dff619b3d6cd6ee41d9466387d694131224
Andy <amonroe805@protonmail.com>
2019-02-18 21:40:16 -0600

    Update

---
+++ test.py
@@ -0,0 +1,13 @@
+from cluster import kmeans
+
+dataset = "datasets/adult.csv"
+xs = "age"
+ys = "capital.gain"
+k = 2
+kmeans(dataset, xs, ys, k)
+
+# dataset = 
+# xs =
+# ys =
+# k =
+# kmeans(dataset, xs, ys, k)
