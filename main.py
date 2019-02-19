700a3d473012d905d7ac987b05b68815758e4059
Gage Benne <gagedbenne@gmail.com>
2019-02-18 21:47:50 -0600

    Group tasks into main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ ys = "PetalLengthCm"
 k = 2
 kmeans(dataset, xs, ys, k)
 
-# dataset = 
-# xs =
-# ys =
-# k =
-# kmeans(dataset, xs, ys, k)
+dataset = "datasets/adult.csv"
+xs = "age"
+ys = "capital.gain"
+k = 2
+kmeans(dataset, xs, ys, k)
